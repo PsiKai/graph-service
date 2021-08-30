bed2e54d602d2254f6b2b4cb2628552a865b7278
David <davidirvin47@gmail.com>
2021-08-30 01:20:40 -0600

    Extract style tag and calculate scale

--- app.js
+++ app.js
@@ -6,13 +6,45 @@ const app = express()
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-app.set('view engine', "ejs")
-
-
 app.get("/", (req, res) => {
-    const {body: {data}, headers: {cookie}} = req
+    const {data} = req.body
     const {graph, points, units} = data
-    ejs.renderFile("./views/graph.ejs", {points, graph, units}, (err, html) => {
+
+    const greatestValue = points.reduce((total, {value}) => {
+        if (total === 0 || value > total) {
+            total = value
+        }
+        return total
+    }, 0) 
+
+    const getScale = () => {
+        const magnitude = Math.round(greatestValue).toString().length
+        switch (magnitude) {
+            case 1:
+                return 10;
+            case 2:
+                return 100;
+            case 3: 
+                if (greatestValue / 2 < 100) {
+                    return 200
+                } else if (greatestValue / 2 < 150) {
+                    return 300
+                } else if (greatestValue / 2 < 200) {
+                    return 400
+                } else if (greatestValue / 2 < 250) {
+                    return 500
+                } else if (greatestValue / 2 < 300) {
+                    return 600
+                }
+                return 1000;
+            default:
+                return;
+        }
+    }
+
+    console.log(getScale());
+
+    ejs.renderFile("./views/graph.ejs", {points, graph, units, scale: getScale()}, (err, html) => {
         if (err) console.log(err);
         res.send(html)
     })
